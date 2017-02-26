15a167f09cc695f3242a2241fcdb8b70c375150b
Dtrain27 <dschroe086@gmail.com>
2017-02-25 20:35:49 -0500

    Create sendSMS.java

---
+++ app/src/main/java/com/dkatzdev/hackbu2017/sendSMS.java
@@ -0,0 +1,74 @@
+package com.dkatzdev.hackbu2017;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class sendSMS extends AppCompatActivity {
+
+    FloatingActionButton send_button = (FloatingActionButton) findViewById(R.id.send_button);
+
+    EditText phone_number = (EditText) findViewById(R.id.phoneNumber);
+
+    EditText message = (EditText) findViewById(R.id.message);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sendsms);
+
+/*
+Creates the click listener for the application and what should happen if the user clicks the send button
+ */
+        send_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                /*
+                Defines and retrieves the phone number and the maessage entered by the user and
+                checks to see if the message is greater
+                 */
+                String phoneNumber = phone_number.getText().toString();
+                String Textmessage = message.getText().toString();
+
+                if (phoneNumber.length() > 0 && Textmessage.length() > 0) {
+                    sendSMS_method(phoneNumber, Textmessage);
+                }
+                /*
+                IF not then we create a Toast object which pop up display that displays an error message
+                 */
+                else{
+                    Toast.makeText(getBaseContext(),
+                                    "Please enter a message and phone number",
+                                    Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+    }
+
+    private void sendSMS_method(String phoneNumber, String Textmessage ){
+        /*
+        This identifies a pending intent object that identifies the object's request and in turn
+        starts up the smsManager and sends the text message
+         */
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, sendSMS.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, Textmessage, pi, null);
+    }
+}
