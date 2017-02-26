119e8be1da283ea3aa61f1e75a5b6694d88bfd05
Dtrain27 <dschroe086@gmail.com>
2017-02-26 01:56:45 -0500

    Create Read_Inbox.java

---
+++ app/src/main/java/com/dkatzdev/hackbu2017/Read_Inbox.java
@@ -0,0 +1,88 @@
+package com.dkatzdev.hackbu2017;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+public class Read_Inbox extends Activity{
+
+    private static final String INBOX_URI = "content://sms/inbox";
+    private static Read_Inbox activity;
+    private TwoLineArrayAdapter<String> smsList;
+    private ListView mListView;
+    Vector<Item> items = new Vector<Item>();
+    Item[] itemArray;
+    private ArrayAdapter<String> adapter;
+    public static Read_Inbox instance() {
+        return activity;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        readSMS();
+        mListView = (ListView) findViewById(R.id.list);
+        mListView.setAdapter(new ItemArrayAdapter(this,itemArray ));
+       // mListView.setAdapter(smsList);
+        mListView.setOnItemClickListener(MyItemClickListener);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        activity = this;
+    }
+    public void readSMS() {
+        ContentResolver contentResolver = getContentResolver();
+        Cursor smsInboxCursor = contentResolver.query(Uri.parse(INBOX_URI), null, null, null, null);
+        int senderIndex = smsInboxCursor.getColumnIndex("address");
+        int messageIndex = smsInboxCursor.getColumnIndex("body");
+        if (messageIndex < 0 || !smsInboxCursor.moveToFirst()) return;
+        adapter.clear();
+        do {
+            String sender = smsInboxCursor.getString(senderIndex);
+            String message = smsInboxCursor.getString(messageIndex);
+            Item item = new Item();
+            item.setPhoneNumber(sender);
+            item.setMessage(message);
+            items.add(item);
+            String formattedText = String.format(getResources().getString(R.string.sms_message), sender, message);
+            adapter.add(Html.fromHtml(formattedText).toString());
+        } while (smsInboxCursor.moveToNext());
+        itemArray = new Item[items.size()];
+        Iterator it = items.iterator();
+        for(int counter = 0; counter < items.size(); counter++){
+            itemArray[counter] = (Item) it.next();
+        }
+    }
+    public void updateList(final String newSms) {
+        adapter.insert(newSms, 0);
+        adapter.notifyDataSetChanged();
+    }
+    private OnItemClickListener MyItemClickListener = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+            try {
+                Toast.makeText(getApplicationContext(), adapter.getItem(pos), Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
+}
